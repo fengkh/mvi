f7a39b53fe1cd8d13c2f8e57d76d4c418c2cca0a
fengkh <fengkh.scu@gmail.com>
2021-11-16 11:40:17 +0800

    2021-11-16-10:40 1.Change the file architecture. 2.Enable to predict the object. 3.The whole process is alright. 4.Packed.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import system_operation.log as log
 import object_detection.functions.prediction as pred
 import object_detection.functions.preprocessing as proc
+import system_operation.log as log
+
+
+# 从病理切片到预测出结果的完整流程操作
+def from_slice_to_result():
+    # ori_path和des_path应为存文件的文件夹路径，并非文件路路径
+    log.log_data("完整流程", 1)
+    proc.compress("D:\_mvi\_ndpi", "D:\_mvi\_ndpi_", 8, 8)
+    proc.normalize_size("D:\_mvi\_ndpi_", "D:\_mvi\_size", 10240)
+    proc.crop("D:\_mvi\_size", "D:\_mvi\_crop", 1024)
+    pred.get_100_predict("D:\_mvi\_crop")
+    proc.stitch("D:\_mvi\_crop", "D:\_mvi\_results")
+
 
 if __name__ == '__main__':
-    log.log_data("完整流程测试-2", 1)
-    # proc.compress("D:\_mvi\_ndpi", "D:\_mvi\_ndpi_", 8, 8)
-    # ori_path和des_path应为存文件的文件夹路径，并非文件路路径
-    # proc.normalize_size("D:\_mvi\_ndpi_", "D:\_mvi\_size", 10240)
-    proc.crop("D:\_mvi\_ndpi", "D:\_mvi\_crop", 1024)
-    pred.get_100_predict("D:\_mvi\_crop")
-    proc.stitch("D:\_mvi\_crop", "D:\_mvi\_stitch")
+    from_slice_to_result()
---
+++ system_operation/license.py
@@ -0,0 +1,11 @@
+import random
+
+from system_operation import log
+
+
+def encryption():
+    log.log_line()
+    log.log_time("License encryption:")
+
+    log.log_time("License encryption complete:")
+    log.log_line()
